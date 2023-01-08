f9a75eb8700368c5b6bb785b2e15b1e06faeef3d
Nicola Downward <nicoladownward@dal.ca>
2023-01-08 19:03:02 -0400

    updated to new player controls

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -1,21 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
+    [SerializeField] InputAction movement;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    void OnEnable() {
+        movement.Enable();
+    }
+
+    void OnDisable() {
+        movement.Disable();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        float horizontalMovement = Input.GetAxis("Horizontal");
+        float horizontalMovement = movement.ReadValue<Vector2>().x;
+        float verticalMovement = movement.ReadValue<Vector2>().y;
 
-        float verticalMovement = Input.GetAxis("Vertical");
+
+        // float horizontalMovement = Input.GetAxis("Horizontal");
+        Debug.Log(horizontalMovement);
+
+        // float verticalMovement = Input.GetAxis("Vertical");
         Debug.Log(verticalMovement);
     }
 }
